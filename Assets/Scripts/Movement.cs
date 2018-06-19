037b5ca317bc2b05feef499ea8c116510394cc14
Mangirish Naik <naikmangirish@gmail.com>
2018-06-20 02:43:04 +0800

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (animator == null) return;
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Move(x, y);
+    }
+
+    private void Move(float X, float Y)
+    {
+        animator.SetFloat("VelX", X);
+        animator.SetFloat("VelY", Y);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+Note:
+This is a simple test example I implemented for 2d Freeform blend trees in Unity3D. 
+The characters is used from standard asset with Idle, Walk & Run cycles of different types.
+The controls are supported in Xbox & PC as well. Building the same controls to Any smartphones will require some cross platform input but doable.
+Controls:
+Keyboard:
+	UP		:	W
+	Down	:	S
+	Left	:	A
+	Right	:	D
